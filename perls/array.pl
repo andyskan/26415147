cf39c0b597e1f33b5be381a1cc10fc3b608e4f7f
Andy Sukanto Kan <m26415147@john.petra.ac.id>
2016-09-28 16:15:20 +0700

    Perl array trying out

---
+++ perls/array.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+@nilai=(100,20,70);
+@nama=(Basuki,Anies,Agus);
+
+print"$nama[0] nilainya $nilai[0]\n";
+print"$nama[1] nilainya $nilai[1]\n";
+print"$nama[2] nilainya $nilai[2]\n";
