a4d2ce195c9712c6c782c73822778b1e31ceb081
Andy Sukanto <m26415147@john.petra.ac.id>
2016-10-19 16:50:37 +0700

    Played string with python, somehow fun

---
+++ python/string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+str = 'Hello Pak Iwan atau Ce Vane <3 !'
+
+print (str)          # Prints complete string
+print (str[0])       # Prints the 'n'th character from string
+print (str[2:8])     # 'n:m' print charahacter from n to m
+print (str[2:])      # Prints string starting from n index character
+print (str * 2)      # Prints string n times
+print (str + "Yang cantik") # Prints string with additional string (concatenated)
