4d6e1abcc487163139e485563c6d7d0cd0d15bf6
Andy Sukanto <m26415147@john.petra.ac.id>
2016-10-19 16:24:41 +0700

    Studying how line and command works in python script

---
+++ python/lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+satu = 1
+dua = 2
+tiga = 3
+
+
+total= satu + \
+	dua + \
+ 	tiga 
+#mengetes cara menggunakan \ pada python, dibuat satu pencobaan per script agar tidak bingung
+
+print(total)
