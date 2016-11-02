dd805a771a4c10b17747d3cf20808126334fa157
Andy Sukanto <m26415147@john.petra.ac.id>
2016-11-02 23:57:04 +0700

    Yeah i hope this works, able to count average i guess

--- latihan/latihan_tes1/data.py
+++ latihan/latihan_tes1/data.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 import json
+import urllib
+rata=[]
+
+for x in range (1,12):
+  urllib.urlretrieve('http://infopangan.jakarta.go.id/api/price/series_by_location?public=1&type=market&lid=7&m='+str(x)+'&y=2015','pangan.json')
+  from pprint import pprint
+  with open('pangan.json') as data_file:
+	data=json.load(data_file)
+  for temp in data['data']:
+	if(type(temp['average'==str):
+      		rata.append(temp['average'])
+                print temp['average'] 
 
 
-from pprint import pprint
+print (sum(rata))
 
-with open('newdata.txt') as data_file:    
-    data = json.load(data_file)
 
-#pprint(data)
 
-for x in data['data']:
-       		 print x['name']
-		 print x['average']
+
--- latihan/latihan_tes1/url.py
+++ latihan/latihan_tes1/url.py
@@ -2,5 +2,6 @@
 
 import json
 import urllib
-urllib.urlretrieve('http://infopangan.jakarta.go.id/api/price/series_by_location?public=1&type=market&lid=7&m=11&y=2016','pangan.json')
+meong=12
+urllib.urlretrieve('http://infopangan.jakarta.go.id/api/price/series_by_location?public=1&type=market&lid='+str(meong)+'7&m=11&y=2015','pangan.json')
 
