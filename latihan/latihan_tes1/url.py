fbba0b7cb6e319ca6da4b36be5a975d8f9bd4730
Andy Sukanto <m26415147@john.petra.ac.id>
2016-11-02 23:05:55 +0700

    NOW ABLE TO RETRIVE URL

--- latihan/latihan_tes1/url.py
+++ latihan/latihan_tes1/url.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
 import json
-import urllib2
-urllib2.urlopen('http://infopangan.jakarta.go.id/api/price/series_by_location?public=1&type=market&lid=7&m=11&y=2016').read()
+import urllib
+urllib.urlretrieve('http://infopangan.jakarta.go.id/api/price/series_by_location?public=1&type=market&lid=7&m=11&y=2016','pangan.json')
 
