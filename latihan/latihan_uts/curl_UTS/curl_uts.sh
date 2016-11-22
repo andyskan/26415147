b92704b66cadd0b1b02903b26617f5d860e9295b
Andy S Kan <m26415147@john.petra.ac.id>
2016-11-22 21:46:21 +0700

    Added some mysql try

---
+++ latihan/latihan_uts/curl_UTS/curl_uts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+curl -s http://bankmandiri.co.id/resource/kurs.asp| grep "USD"|grep -o '[0-9]*'|xargs|awk '{printf" Harga Beli : %d\n Harga Jual : %d\n Selisih : %d\n",$1,$2,$2-$1}'
